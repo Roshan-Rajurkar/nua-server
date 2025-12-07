fa1bbe5ba1e23c189f19572428ee78a23151abb2
Roshan-Rajurkar <roshangit50@gmail.com>
2025-12-07 18:19:25 +0530

    resolve iisue

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const getUserId = (req, res, next) => {
-  console.log("Came for verification");
   const authHeader = req.header("Authorization");
   if (!authHeader) return res.status(401).json({ error: "Unauthorized" });
 
